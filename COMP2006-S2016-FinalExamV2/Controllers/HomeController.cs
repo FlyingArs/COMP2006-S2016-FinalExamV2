e1fae8c4eefdfa7d49d321dd11c67cda1a2f8551
FlyingArs <davidyu89tech@gmail.com>
2016-08-18 14:35:24 -0400

    Added Controllers & internal documentations

--- COMP2006-S2016-FinalExamV2/Controllers/HomeController.cs
+++ COMP2006-S2016-FinalExamV2/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿/** Authors & Student Number:
+    Siqian Yu 200286902
+    Date Modified: 08-18-2016
+    Short Version History: This is a Home Controller. 
+    File Description: The home controller decides what to do wth the home view. 
+    **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-// DY
 
 namespace COMP2006_S2016_FinalExamV2.Controllers
 {
